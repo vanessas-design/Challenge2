1b40b1dd8a927d96523bcb3e14e670e980787cc7
vanessas-design <vanessa.seymour@knights.ucf.edu>
2020-03-08 23:56:12 -0400

    Challenge2

---
+++ Challenge2/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+
+    // Transforms to act as start and end markers for the journey.
+    public Transform startMarker;
+    public Transform endMarker;
+
+    // Movement speed in units/sec.
+    public float speed = 1.0F;
+
+    // Time when the movement started.
+    private float startTime;
+
+    // Total distance between the markers.
+    private float journeyLength;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Keep a note of the time the movement started.
+        startTime = Time.time;
+
+        // Calculate the journey length.
+        journeyLength = Vector2.Distance(startMarker.position, endMarker.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Distance moved = time * speed.
+        float distCovered = (Time.time - startTime) * speed;
+
+        // Fraction of journey completed = current distance divided by total distance.
+        float fracJourney = distCovered / journeyLength;
+
+        // Set our position as a fraction of the distance between the markers and pingpong the movement.
+        transform.position = Vector2.Lerp(startMarker.position, endMarker.position, Mathf.PingPong(fracJourney, 1));
+    }
+}
---
+++ Challenge2/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class PlayerScript : MonoBehaviour
+{
+    public Text countText;
+    public Text winText;
+    public Text livesText;
+    public Text restartText;
+    public AudioSource musicSource;
+    public AudioClip musicClipOne;
+    public AudioClip musicClipTwo;
+
+
+    public float speed;
+
+    private Rigidbody2D rd2d;
+    private int count;
+    private int lives;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rd2d = GetComponent<Rigidbody2D>();
+        count = 0;
+        lives = 3;
+        SetCountText();
+        SetLivesText();
+        winText.text = "";
+        restartText.text = "";
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float hozMovement = Input.GetAxis("Horizontal");
+        float vertMovement = Input.GetAxis("Vertical");
+
+        rd2d.AddForce(new Vector2(hozMovement * speed, vertMovement * speed));
+
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
+        if (Input.GetKey("r"))
+        {
+            SceneManager.LoadScene(0);
+        }
+        countText.text = "Count:" + count.ToString(); 
+        if (count >= 0)
+        {
+            musicSource.clip = musicClipOne;
+            musicSource.Play();
+        }
+        if (count ==8)
+        {
+            musicSource.clip = musicClipTwo;
+            musicSource.Play(); 
+        }
+
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Coin")
+        {
+            count = count + 1;
+            SetCountText();
+            Destroy(collision.collider.gameObject);
+        }
+        else if (collision.collider.tag == "Enemy")
+        {
+            Destroy(collision.collider.gameObject);
+            lives = lives - 1;
+            SetLivesText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count:" + count.ToString();
+        if (count == 8)
+        {
+            winText.text = "You Win! Game created by Vanessa Seymour!";
+        }
+        if (count == 4)
+        {
+            transform.position = new Vector2(61.1f, .1f);
+            lives = 3;
+            livesText.text = "Lives:" + lives.ToString();
+        }
+    }
+
+    void SetLivesText()
+    {
+        livesText.text = "Lives:" + lives.ToString();
+        if (lives == 0)
+        {
+            winText.text = "Game Over!";
+            DestroyScriptInstance();
+            restartText.text = "Press 'R' to try again";
+        }
+
+        void DestroyScriptInstance()
+        {
+            Destroy(this.gameObject);
+        }
+    }
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Ground")
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                rd2d.AddForce(new Vector2(0, 3), ForceMode2D.Impulse);
+            }
+
+        }
+    }
+    
+}
